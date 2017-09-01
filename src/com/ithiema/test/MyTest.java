e546f8daef0486d86e7ccc87304675bd9661de27
沉默的大多数 <forgem1229@163.com>
2017-09-01 18:32:37 +0800

    Initial commit

---
+++ src/com/ithiema/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.ithiema.test;
+
+/**
+ * Created by Administrator on 2017/8/30.
+ */
+
+public class MyTest {
+    public static void main(String[] args) {
+        int[] arr = {11, 22, 33, 44, 15};
+        for (int i = 0; i <arr.length-1 ; i++) {
+            System.out.println("这是我的第一个hello world");
+         }
+    }
+}
